78433560d02521758c6b7e954bc057b2054f42c1
linuxrafiq <rafiq.i@live.co.uk>
2020-05-07 11:54:11 +0600

    created modele for search object

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-// Global app controller
+import Search from './models/Search'
+/** Global state of the app
+ * -Search object
+ * -Current recipe object
+ * -Shopping list object
+ * -Liked recipes
+ */
+const state = {};
+const controlSearch =()=>{
+  //Get the query from view
+  const query = 'pizza';
+  if (query){
+    // New search object and add to state
+    state.search = new Search(query); 
+  }
+}
+document.querySelector('.search').addEventListener('submit', event=>{
+  event.preventDefault();
+  controlSearch();
+});
+const search = new Search('pizza');
+console.log(search);
+console.log(search.getResults()); 

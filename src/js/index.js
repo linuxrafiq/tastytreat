7d117104a727222bb106378190bb659b9b3d79c3
linuxrafiq <rafiq.i@live.co.uk>
2020-05-07 17:32:51 +0600

    build search controller

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,20 @@ import Search from './models/Search'
  * -Liked recipes
  */
 const state = {};
-const controlSearch =()=>{
+const controlSearch = async()=>{
   //Get the query from view
   const query = 'pizza';
   if (query){
     // New search object and add to state
     state.search = new Search(query); 
+
+    //Prepare UI for results
+
+    //Search for recipes
+    await state.search.getResults();
+
+    // render results on UI
+    console.log(state.search.result );
   }
 }
 //event listner to the parent object to delegate the event
@@ -19,6 +27,3 @@ document.querySelector('.search').addEventListener('submit', event=>{
   event.preventDefault();
   controlSearch();
 });
-const search = new Search('pizza');
-console.log(search);
-console.log(search.getResults()); 

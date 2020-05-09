a9fb51086625fbee76c0bf5d6abbe726db5bc308
linuxrafiq <rafiq.i@live.co.uk>
2020-05-09 08:06:46 +0600

    pagination complete in search list

--- src/js/index.js
+++ src/js/index.js
@@ -39,18 +39,11 @@ element.searchForm.addEventListener('submit', event=>{
 
 element.searchResPages.addEventListener('click', e=>{
   //Starting with the Element itself, the closest() method traverses parents (heading toward the document root) of the Element until it finds a node that matches the provided selectorString. Will return itself or the matching ancestor. If no such element exists, it returns null.
-  console.log('btnclicke');
   const btn = e.target.closest('.btn-inline');
-  console.log('btnclicke1');
   if(btn){
-    
-    console.log('btnclicke2');
     const goToPage = parseInt(btn.dataset.goto, 10);//base 10
     searchView.clearResults();
-    console.log('goToPage'+btn.dataset.goto);
     searchView.renderResults(state.search.result, goToPage);
-    console.log('btnclicke4');
-
   }
 });
 

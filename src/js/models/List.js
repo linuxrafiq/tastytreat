ebd635f10ebcef4d6cec686cf9daa3c7742ac2a4
linuxrafiq <rafiq.i@live.co.uk>
2020-05-10 16:42:21 +0600

    list function addItem, deleteItem, updateCount

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,3 +1,4 @@
+import uniqid from 'uniquid'
 export default class List{
     constructor(){
        this.items = []; 
@@ -5,9 +6,22 @@ export default class List{
 
     addItem(count, unit, ingredient){
         const item = {
+            id:uniqid(),
             count,
             unit,
             ingredient
         }
+    }
+
+    deleteItem(id){
+        const index = this.items.findIndex(el=>el.id === id);
+        //splice[startPos,totalElement]  
+        //[2,4,8] splice[1,1]// it will start from position 1 and remove only one element starting from  position 1
+        //return 4 onrignal array will become [2,8]
+        return this.items.splice(index, 1)
+    }
+
+    updateCount(id, newCount){
+        this.items.find(el=>el.id===id).count = newCount;
     }
 }
